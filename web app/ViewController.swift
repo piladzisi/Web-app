c3e8e0dcd9e24a471de5c49f51a110b8d31c0648
Anna <piladzisi@gmail.com>
2019-04-02 11:46:44 +0300

    working

--- web app/ViewController.swift
+++ web app/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let url = URL(string: "https://www.google.com")
+        let url = URL(string: "https://www.google.com/en")
         let request = URLRequest(url: url!)
         webView.load(request)
     }
