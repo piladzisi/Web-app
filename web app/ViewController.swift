b2624665bb58f435681fac639138fd050376cd33
Anna <piladzisi@gmail.com>
2019-04-02 09:51:25 +0300

    Web search app

---
+++ web app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  web app
+//
+//  Created by anna.sibirtseva on 02/04/2019.
+//  Copyright © 2019 anna.sibirtseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
